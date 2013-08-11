0b808324835f324d00e40ec561f29d2312672a8e
Y.Okamura <okamura@informationsea.info>
2013-08-11 22:06:03 +0900

    update

--- env.sh
+++ env.sh
@@ -2,10 +2,15 @@ export PATH=export PATH=/data/toolchain/bin:$PATH
 
 export PREFIX=/data/data/jackpal.androidterm/app_HOME/local
 export CC=arm-linux-androideabi-gcc
+export LD=arm-linux-androideabi-ld
 export CXX=arm-linux-androideabi-c++
 export AR=arm-linux-androideabi-ar
 export RANLIB=arm-linux-androideabi-ranlib
-export HOST=arm-linux-androideabi
+export HOSTCONFIG=arm-linux-androideabi
 
 mkdir -p archives
 mkdir -p build
+
+function android-configure() {
+  ./configure --prefix $PREFIX --host $HOSTCONFIG "$@"
+}
